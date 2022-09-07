e90d8d036dbbfececb7997174037110a491dbbae
Ahmed Mujtba <ahmedmujtba@ahmeds-mbp.home>
2022-09-07 07:24:42 +0100

    Get reviews by id completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -29,3 +29,35 @@ describe("/api/categories", () => {
     });
   });
 });
+describe("/api/reviews/:review_id", () => {
+  describe("GET", () => {
+    test("200: should return a review object with following properties: review_id, title, review_body, designer, review_img_url, votes, category (references slug in categories), owner (references users primary key i.e. username), created_at ", () => {
+      return request(app)
+        .get("/api/reviews/6")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.review).toBeInstanceOf(Object);
+          expect(body.review).toHaveProperty("review_id", expect.any(Number));
+          expect(body.review).toHaveProperty("title", expect.any(String));
+          expect(body.review).toHaveProperty("review_body", expect.any(String));
+          expect(body.review).toHaveProperty("designer", expect.any(String));
+          expect(body.review).toHaveProperty(
+            "review_img_url",
+            expect.any(String)
+          );
+          expect(body.review).toHaveProperty("votes", expect.any(Number));
+          expect(body.review).toHaveProperty("category", expect.any(String));
+          expect(body.review).toHaveProperty("owner", expect.any(String));
+          expect(body.review).toHaveProperty("created_at", expect.any(String));
+        });
+    });
+    test("404: should return an error when review_id passed does not exist", () => {
+      return request(app)
+        .get("/api/reviews/9999")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("Not Found");
+        });
+    });
+  });
+});
--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const { getCategories } = require("./controllers/categories.controllers");
+const { getReviewById } = require("./controllers/reviews.controllers");
 const app = express();
+app.use(express.json());
 
 app.get("/api/categories", getCategories);
+app.get("/api/reviews/:review_id", getReviewById);
 
-//Error Handlers
+// Error Handlers
+
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  let psqlErrors = "22P02";
+  if (psqlErrors.includes(err.code)) {
     res.status(400).send({ msg: "Bad Request" });
   } else {
     next(err);
